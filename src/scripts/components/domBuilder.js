776ec6dfdeae4250ca61a9ff09e638a9490e4e0f
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-06-04 12:24:02 -0500

    add domBuilder function

---
+++ src/scripts/components/domBuilder.js
@@ -0,0 +1,13 @@
+const domBuilder = () => {
+  const domString = `
+  <div id="navigation"></div>
+  <div id="main-container">
+    <div id="filter-buttons"></div>
+    <div id="form-container"></div>
+    <div id="card-container"></div>
+  </div>`;
+
+  renderToDOM('#app', domString);
+};
+
+export default domBuilder;
