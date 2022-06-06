53bac00f8bd0d2b09a7926987756c38e9349e317
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-06-06 15:20:44 -0500

    added click event to navEvents to have form show on dom

--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -4,12 +4,14 @@ import navBar from '../components/navBar';
 import filterButttons from '../components/filterButtons';
 import { showCards } from '../components/cardsPage';
 import logoutButton from '../components/logoutButton';
+import navEvents from '../components/Events/navEvents';
 
 const startApp = () => {
   domBuilder();
   navBar();
   filterButttons();
   logoutButton();
+  navEvents();
   getCards().then((cardsArray) => showCards(cardsArray));
 };
 
