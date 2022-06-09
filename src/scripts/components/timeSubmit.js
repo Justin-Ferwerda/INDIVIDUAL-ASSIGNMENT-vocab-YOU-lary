8f90fcb3c8323de500d6cd3dafa10b86bc5200ca
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-06-08 19:42:32 -0500

    refactor timeSubmit function to show month first

--- src/scripts/components/timeSubmit.js
+++ src/scripts/components/timeSubmit.js
@@ -1,6 +1,6 @@
 const time = () => {
   const today = new Date();
-  const date = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const date = `${today.getMonth() + 1}-${today.getDate()}-${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
   return date;
 };
 
